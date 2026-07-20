90fbb24bb645ea1509d7d97cf2e16d907eef8648
belousov dmitry <da_belousov@list.ru>
2026-07-20 19:58:39 +0300

    user template

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Http\Controllers\Admin\MainController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\User\UserIndexController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/admin', [MainController::class, 'index'])->name('admin.index');
+Route::get('/', [UserIndexController::class, 'index'])->name('user.index');
+Route::get('/register', [RegisterController::class, 'create'])->name('user.register');
+Route::get('/login', [LoginController::class, 'create'])->name('user.login');
